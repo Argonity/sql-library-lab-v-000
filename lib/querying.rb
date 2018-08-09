e2728763484e3085605148c3ed6c3a08fac73a74
Maurice Argoetti <maurice.argoetti@gmail.com>
2018-08-09 12:25:44 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,8 +30,7 @@ def select_name_and_series_subgenres_of_authors
   ON authors.id = subgenres.id
   GROUP BY authors.name
   ORDER BY subgenres.name
-  LIMIT 2
-  "
+  LIMIT 2"
 end
 
 def select_series_title_with_most_human_characters
