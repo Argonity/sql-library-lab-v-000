79eeaee93508e45ca13c75adfe93b2c7efc65475
Maurice Argoetti <maurice.argoetti@gmail.com>
2018-08-09 12:25:47 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,8 +39,7 @@ def select_series_title_with_most_human_characters
   JOIN characters
   ON series.id = characters.series_id
   GROUP BY series.title
-  LIMIT 1
-  "
+  LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
